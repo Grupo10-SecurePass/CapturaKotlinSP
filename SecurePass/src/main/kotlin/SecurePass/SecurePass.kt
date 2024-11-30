7da8541187dad0abd83cb7714bc74fc82eaf0296
Ayrton Casa <ayrton.casa@sptech.school>
2024-11-30 15:45:39 -0300

    Testando sistema

--- SecurePass/src/main/kotlin/SecurePass/SecurePass.kt
+++ SecurePass/src/main/kotlin/SecurePass/SecurePass.kt
@@ -29,9 +29,9 @@ class SecurePass {
     fun configurar() {
         val datasource = BasicDataSource()
         datasource.driverClassName = "com.mysql.cj.jdbc.Driver"
-        datasource.url = "jdbc:mysql://localhost:3306/securepass?serverTimezone=America/Sao_Paulo"
+        datasource.url = "jdbc:mysql://3.83.176.75:3306/securepass?serverTimezone=America/Sao_Paulo"
         datasource.username = "root"
-        datasource.password = "#Gf48500284897"
+        datasource.password = "urubu100"
 
         jdbcTemplate = JdbcTemplate(datasource)
     }
